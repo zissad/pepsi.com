44a2911201aa121b1833c8ea4344ecb5d7d404c0
zissad <88398384+zissad@users.noreply.github.com>
2021-10-21 18:22:29 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function imgSlider(anything){
+    document.querySelector('.pepsi').src = anything;
+}
+function changeBgColor(color){
+    document.querySelector('.sec').style.background = color;
+}
+function menuToggle() {
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const navigation = document.querySelector('.navigation');
+     toggleMenu.classList.toggle('active')
+     navigation.classList.toggle('active')
+}
